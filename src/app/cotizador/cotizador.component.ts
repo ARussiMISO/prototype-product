cfdfe0c72ade66560f52eeb432080808b1541c16
Fredy Alejandro Russi Fajardo <frussi@asesoftware.com>
2024-03-14 01:31:34 -0500

    Avance cotizador

--- src/app/cotizador/cotizador.component.ts
+++ src/app/cotizador/cotizador.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator, MatPaginatorIntl } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { ConfiguracionPaginacion } from 'src/shared/paginator';
 
 @Component({
   selector: 'app-cotizador',
   templateUrl: './cotizador.component.html',
-  styleUrls: ['./cotizador.component.css']
+  styleUrls: ['./cotizador.component.css'],
+  providers: [
+    { provide: MatPaginatorIntl, useValue: ConfiguracionPaginacion() }],
 })
 export class CotizadorComponent implements OnInit {
-
+  @ViewChild('paginator') paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+  dataSource = new MatTableDataSource<Elemento>([]);
+  displayedColumns: string[] = ['tipo', 'alto', 'ancho', 'profundidad', 'accion'];
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  agregarElemento(){
+
+  }
+
+  cotizar(){
+
+  }
+
+  modificarElemento(elemento: Elemento){
+
+  }
+  eliminarElemento(elemento: Elemento){
+
+  }
+
+
+}
+
+interface Elemento {
+  tipo?: string;
+  alto?: number;
+  ancho?: number;
+  profundidad?: number;
 }
